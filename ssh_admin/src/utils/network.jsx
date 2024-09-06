6c92393f585bfcf3cd2aec5b8afbc43f9866763f
shine logics <shinelogics19@gmail.com>
2024-09-07 00:07:06 +0530

    new build generated for development

--- ssh_admin/src/utils/network.jsx
+++ ssh_admin/src/utils/network.jsx
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:8080/";
-const BASE_URL = "http://139.59.17.95:8080/";
+const BASE_URL = "https://cloud.kalindainfotech.com/";
 
 export const LOGINAPI = BASE_URL + "login";
 export const USERSAPI = BASE_URL + "users";
