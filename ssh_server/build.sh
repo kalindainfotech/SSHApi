c3635e53ff173673188d220d0445c56984c67330
svadhyaya <svadhyaya1410@gmail.com>
2024-09-11 16:56:41 +0530

    [updated build.sh]

--- ssh_server/build.sh
+++ ssh_server/build.sh
@@ -2,9 +2,9 @@ Linux:
  only-cli:
      nuitka --standalone --static-libpython=yes --output-dir=dist/linux --onefile ssh_client.py
  gui-cli:
-     nuitka --standalone --static-libpython=yes --output-dir=dist/linux-gui --onefile ssh_client_gui.py
+     nuitka --standalone --static-libpython=yes --output-dir=dist/linux-gui --enable-plugin=tk-inter --onefile ssh_client_gui.py
 Windows:
  only-cli:
-     nuitka --standalone --static-libpython=yes --output-dir=dist/windows --onefile ssh_client.py
+     nuitka --standalone --mingw64 --output-dir=dist/windows --onefile ssh_client.py
  gui-cli:
-     nuitka --standalone --static-libpython=yes --output-dir=dist/windows-gui --onefile ssh_client_gui.py
+     nuitka --standalone --mingw64 --output-dir=dist/windows-gui --enable-plugin=tk-inter --onefile ssh_client_gui.py
